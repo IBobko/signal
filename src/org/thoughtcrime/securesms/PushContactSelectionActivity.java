9092cd6a26ef07cfe8746859d7cc1a9990351486
Igor Bobko <limit-speed@yandex.ru>
2016-10-27 08:05:18 +0300

    Add functionality for conversation

--- src/org/thoughtcrime/securesms/PushContactSelectionActivity.java
+++ src/org/thoughtcrime/securesms/PushContactSelectionActivity.java
@@ -38,23 +38,8 @@ public class PushContactSelectionActivity extends ContactSelectionActivity {
 
   @Override
   protected void onCreate(Bundle icicle, @NonNull MasterSecret masterSecret) {
-    getIntent().putExtra(ContactSelectionListFragment.MULTI_SELECT, true);
-    super.onCreate(icicle, masterSecret);
-
-    getToolbar().setNavigationIcon(R.drawable.ic_check_white_24dp);
-    getToolbar().setNavigationOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        Intent resultIntent = getIntent();
-        List<String> selectedContacts = contactsFragment.getSelectedContacts();
-
-        if (selectedContacts != null) {
-          resultIntent.putStringArrayListExtra("contacts", new ArrayList<>(selectedContacts));
-        }
-
-        setResult(RESULT_OK, resultIntent);
-        finish();
-      }
-    });
+//
+//      }
+//    });
   }
 }
