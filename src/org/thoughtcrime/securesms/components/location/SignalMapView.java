093555a44b4181795554a3e30d1bcf494d0672d6
Stepanov Vyacheslav <v.stepanov@innopolis.ru>
2016-11-02 23:10:05 +0300

    Add firebase to project

--- src/org/thoughtcrime/securesms/components/location/SignalMapView.java
+++ src/org/thoughtcrime/securesms/components/location/SignalMapView.java
@@ -12,7 +12,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.google.android.gms.location.places.Place;
+//import com.google.android.gms.location.places.Place;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
